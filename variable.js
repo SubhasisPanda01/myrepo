8ed4e3165ab56cfc9a4f44c97158a365a88ef347
SubhasisPanda01 <subhasisnist@gmail.com>
2023-02-28 15:04:31 +0530

    variables

--- variable.js
+++ variable.js
@@ -27,3 +27,43 @@ console.log(10 =="10");
 console.log(10 ==="10");
 console.log(10=="ten");
 console.log(10==="ten");
+
+
+var decimalnum=100;
+var doublenum=100.123;
+var hexadecimalnum=0x123456789ABCDEF;
+var octalnum=0o1234567;
+var binarynum=0b10011010;
+console.log(decimalnum,doublenum,hexadecimalnum,octalnum,binarynum);
+
+var flag=true;
+console.log(flag);
+
+var arr1=[10,20,30,40,50];
+arr1.forEach(function(element,index){
+console.log(element,index);
+});
+
+
+var front_end=["Angular","ReactJS","vueJS"];
+var databases=["Oracle","MySQL","MongoDB"];
+front_end.forEach((element,index)=>{
+    console.log(element,databases[index]);
+});
+
+
+var obj={sub_one:"Angular11",
+         sub_two:"NodeJS",
+         sub_three:"MongoDB"
+        };
+console.log(obj.sub_one,obj.sub_two,obj.sub_three);
+
+
+for (var key in obj) {
+    console.log(key,obj[key]);
+     };
+
+
+
+
+
